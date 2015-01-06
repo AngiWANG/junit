50c237b90635ff58d919d097e8351fad1914bcb3
Angi WANG <angi.wang.afe@gmail.com>
2015-01-06 09:06:08 +0800

    solve generic

--- src/test/java/com/angi/junit/TestParameter.java
+++ src/test/java/com/angi/junit/TestParameter.java
@@ -12,13 +12,12 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 /**
- * @author Administrator
- * 第一个注意点
+ * @author Administrator 第一个注意点
  */
 @RunWith(Parameterized.class)
 public class TestParameter {
 	private static Money f12CHF;
-	//	第二个注意点
+	// 第二个注意点
 	private Money expected;
 	private Money target;
 
@@ -28,11 +27,12 @@ public class TestParameter {
 	}
 
 	/**
-	 *  第三个注意点
+	 * 第三个注意点
+	 * 
 	 * @return
 	 */
 	@Parameters
-	public static Collection words() {
+	public static Collection<Object[]> words() {
 		return Arrays.asList(new Object[][] { { new Money(23, "CHF"), new Money(11, "CHF") },
 				{ new Money(28, "CHF"), new Money(16, "CHF") }
 
@@ -40,19 +40,19 @@ public class TestParameter {
 	}
 
 	/**
-	 * 第四个注意点
-	*参数化测试必须的构造函数
-	*@paramexpected   期望的测试结果，对应参数集中的第一个参数
-	*@paramtarget 测试数据，对应参数集中的第二个参数
-	*/
+	 * 第四个注意点 参数化测试必须的构造函数
+	 * 
+	 * @paramexpected 期望的测试结果，对应参数集中的第一个参数
+	 * @paramtarget 测试数据，对应参数集中的第二个参数
+	 */
 	public TestParameter(Money expected, Money target) {
 		this.expected = expected;
 		this.target = target;
 	}
 
 	/**
-	  *实际需要测试的方法
-	  */
+	 * 实际需要测试的方法
+	 */
 	@Test
 	public void add() {
 		assertTrue(expected.equals(f12CHF.add(target)));
